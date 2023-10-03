87c684ffee96001ef1a589380bcb1d22ac3f64b0
pidorok228rus <118135432+farcis28800@users.noreply.github.com>
2023-10-03 16:32:21 +0300

    GmailGenerate
    Generator gmail.com written in pure python with the random library!

---
+++ index.py
@@ -0,0 +1,17 @@
+import random
+
+gmail = 'qwertyuiopasdfghjkllzxcvbnmm1234567890'
+quantity_gmail = int(input('quantity gmail: '))
+length_gmail = int(input('length gmail: '))
+length_password = int(input('length password: '))
+
+
+
+for n in range(quantity_gmail):
+    password =''
+    gmails =''
+    for i in range(length_gmail):
+        gmails += random.choice(gmail)
+    for i in range(dlinap):
+        password += random.choice(gmail)
+    print(gmails + '@gmail.com', password)
